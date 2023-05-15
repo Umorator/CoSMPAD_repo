0dac16a9267f730b15f8e8c86c942b2e474a77c3
Rafael <rafaelumt@outlook.es>
2023-05-15 13:52:17 +0200

    CoMSPAD

--- 1_Home.py
+++ 1_Home.py
@@ -27,11 +27,11 @@ st.title("About")
 st.write("CoMSPAD encompasses <N> types of experimental information found in <N> proteins by using different signal peptides which aims to serve as a reference for the recombinant protein production community, providing a simple interface for the extraction of data related to the expression of a target protein.")
 
 
-a = pd.DataFrame(df.Specie.value_counts()).reset_index()
-a.columns = ['Specie','Values']
+a = pd.DataFrame(df.Species.value_counts()).reset_index()
+a.columns = ['Species','Values']
 
 st.title('Statistics')
-fig = px.pie(a.head(15), values='Values', names='Specie', title='Expression Specie Distribution')
+fig = px.pie(a.head(15), values='Values', names='Species', title='Expression Species Distribution')
 st.plotly_chart(fig)
 
 b = pd.DataFrame(df['Protein name'].value_counts()).reset_index()
