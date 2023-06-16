ca6a96e1069f92faf3d4680cfe4a0e79fdd5e2d3
Rafael <rafaelumt@outlook.es>
2023-06-16 18:53:58 +0200

    figure size

--- 1_Home.py
+++ 1_Home.py
@@ -28,30 +28,20 @@ st.write("CoSMPAD encompasses more than 20 types of experimental information fou
 
 
 #Species distribution -----------------------------------
+st.title('Statistics')
+
+col1, col2, col3 = st.columns([0.3, 1, 0.3])
 
 a = pd.DataFrame(df.Species.value_counts()).reset_index()
 a.columns = ['Species','Values']
 
-st.title('Statistics')
-col1, col2= st.columns([1,1])
 
 fig = px.pie(a, values='Values', names='Species', title='Species Distribution')
 fig.update_traces(textposition='inside')
-col1.plotly_chart(fig, use_container_width=True)
-
-
-#SP type distribution ------------------------------------
-
-d = pd.DataFrame(df['SP type'].value_counts()).reset_index()
-d.columns = ['SP type','Values']
-
-fig4 = px.pie(d, values='Values', names='SP type', title='SP type Distribution')
-fig4.update_traces(textposition='inside')
-
-col2.plotly_chart(fig4, use_container_width=True)
+col2.plotly_chart(fig)
 
 #Protein distribution ------------------------------------
-col1, col2= st.columns([1,1])
+col1, col2, col3 = st.columns([0.3, 1, 0.3])
 
 b = pd.DataFrame(df['Protein name'].value_counts()).reset_index()
 b.columns = ['Proteins','Values']
@@ -59,16 +49,30 @@ b.columns = ['Proteins','Values']
 fig2 = px.pie(b, values='Values', names='Proteins', title='Proteins Distribution')
 fig2.update_traces(textposition='inside')
 
-col1.plotly_chart(fig2, use_container_width=True)
+col2.plotly_chart(fig2)
+
 
 #Promoter distribution ------------------------------------
+col1, col2, col3 = st.columns([0.3, 1, 0.3])
 
 c = pd.DataFrame(df['Promoter'].value_counts()).reset_index()
 c.columns = ['Promoter','Values']
 
 fig3 = px.pie(c, values='Values', names='Promoter', title='Promoter Distribution')
 fig3.update_traces(textposition='inside')
-col2.plotly_chart(fig3, use_container_width=True)
+col2.plotly_chart(fig3)
+
+
+#SP type distribution ------------------------------------
+col1, col2, col3 = st.columns([0.3, 1, 0.3])
+
+d = pd.DataFrame(df['SP type'].value_counts()).reset_index()
+d.columns = ['SP type','Values']
+
+fig4 = px.pie(d, values='Values', names='SP type', title='SP type Distribution')
+fig4.update_traces(textposition='inside')
+
+col2.plotly_chart(fig4)
 
 
 
